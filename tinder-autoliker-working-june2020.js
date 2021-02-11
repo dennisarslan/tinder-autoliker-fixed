2c7280383f4b6141dc3889fa3806cc05fd49de5a
Dennis Arslan <dennis@arslan.co.uk>
2021-02-11 17:33:50 -0500

    Update tinder-autoliker-working-june2020.js

--- tinder-autoliker-working-june2020.js
+++ tinder-autoliker-working-june2020.js
@@ -55,7 +55,8 @@ function trickTinder(runnumber) {
 
   console.log(runnumber)
   if(runnumber=="firstrun") {
-    var like = document.getElementsByClassName("button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand D(b) Bgc(#fff) Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a")[1];
+    var like = document.getElementsByClassName("button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand D(b) Bgc(#fff) Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a")[2];
+    // var like = document.getElementsByClassName("button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand D(b) Bgc(#fff) Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a")[1];
   }
 
   // Open profile bio
